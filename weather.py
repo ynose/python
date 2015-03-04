b2248c872f8d6f0b9d0a091fa505234c58f2cbdb
ynose_fsd <nose@fsd-jp.com>
2015-03-04 17:55:30 +0900

    jsonを直接操作して取得

--- weather.py
+++ weather.py
@@ -15,15 +15,11 @@ def do_json(s):
     item_list = json.loads(s)
     
 
-    for  k, v in item_list.iteritems():
-        if k == "title":
-            print urllib.unquote(v.encode('utf8'))
-        if k == "forecasts":
-            print urllib.unquote(v[1]['dateLabel'].encode('utf8'))
-            print urllib.unquote(v[1]['date'].encode('utf8'))
-            print urllib.unquote(v[1]['telop'].encode('utf8'))
-            print urllib.unquote(v[1]['temperature']['min']['celsius'].encode('utf8'))
-            print urllib.unquote(v[1]['temperature']['max']['celsius'].encode('utf8'))
+    print urllib.unquote(item_list["title"].encode('utf8'))
+    print urllib.unquote(item_list["forecasts"][1]['dateLabel'].encode('utf8'))
+    print urllib.unquote(item_list["forecasts"][1]['date'].encode('utf8'))
+    print urllib.unquote(item_list["forecasts"][1]['temperature']['min']['celsius'].encode('utf8'))
+    print urllib.unquote(item_list["forecasts"][1]['temperature']['max']['celsius'].encode('utf8'))
 
 if __name__ == '__main__':
     json_str = weather_api()
