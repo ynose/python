5b7f6c33c43ba9c5b0075c8f5d31e71cc871c9a5
ynose <ynose.249@gmail.com>
2015-03-16 23:07:19 +0000

    今日の予定に変更

--- weather.py
+++ weather.py
@@ -4,24 +4,31 @@ import sys
 import urllib
 import json
 
-def weather_api():
+def livedoor_weather_api():
     url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=120010'
 
     #print url + params
     response = urllib.urlopen(url)
     return response.read()
     
-def do_json(s):
+def livedoor_weather_json(s):
     item_list = json.loads(s)
     
-
+    # 場所名
     print urllib.unquote(item_list["title"].encode('utf8'))
-    print urllib.unquote(item_list["forecasts"][1]['dateLabel'].encode('utf8'))
-    print urllib.unquote(item_list["forecasts"][1]['date'].encode('utf8'))
-    print urllib.unquote(item_list["forecasts"][1]['temperature']['min']['celsius'].encode('utf8'))
-    print urllib.unquote(item_list["forecasts"][1]['temperature']['max']['celsius'].encode('utf8'))
+    # 今日[0]  明日は[1]  明後日は[2]
+    forecasts_today = item_list["forecasts"][0]
+    print urllib.unquote(forecasts_today['dateLabel'].encode('utf8'))
+    print urllib.unquote(forecasts_today['date'].encode('utf8'))
+    print urllib.unquote(forecasts_today['telop'].encode('utf8'))
+    # 最低気温
+    if forecasts_today['temperature']['min'] is not None:
+        print "最低気温" + urllib.unquote(forecasts_today['temperature']['min']['celsius'].encode('utf8')) + "℃"
+        
+    if forecasts_today['temperature']['max'] is not None:
+        print "最高気温" + urllib.unquote(forecasts_today['temperature']['max']['celsius'].encode('utf8')) + "℃"
 
 
 if __name__ == '__main__':
-    json_str = weather_api()
-    do_json(json_str)
+    json_str = livedoor_weather_api()
+    livedoor_weather_json(json_str)
